882c948bbd780e6686a0122e974ac2b8f6613c90
furkankaratas <info@furkankaratas.com.tr>
2026-03-01 01:33:48 +0300

    first integration

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,5 +1,5 @@
 // db.js
-require('dotenv').config();
+//require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 
 // MongoDB connection URL with authentication options
@@ -16,13 +16,13 @@ async function connectToDatabase() {
     const client = new MongoClient(url);      
 
     // Task 1: Connect to MongoDB
-    // {{insert code}}
+    await client.connect();
 
     // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+    dbInstance = client.db(dbName);
 
     // Task 3: Return database instance
-    // {{insert code}}
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
